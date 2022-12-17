227c7deb22becca405b1aac9cadb44e9edef15dc
serjoradpytnio <serjoradpytnio@mail.ru>
2022-12-17 20:40:50 +0300

    Add test_circle.py

--- test_circle.py
+++ test_circle.py
@@ -4,10 +4,10 @@ from math import pi
 
 class TestCircleArea(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(circle_area(3),pi*3**2)
-        self.assertEqual(circle_area(1),pi)
-        self.assertEqual(circle_area(0),0)
-        self.assertEqual(circle_area(2,5),pi*2,5**2)
+        self.assertEqual(circle_area(3), pi*3**2)
+        self.assertEqual(circle_area(1), pi)
+        self.assertEqual(circle_area(0), 0)
+        self.assertEqual(circle_area(2.5), pi*2.5**2)
 
     def test_values(self):
         self.assertRaises(ValueError, circle_area, -2)
