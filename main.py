8fa488ecad0c4592203cac3efe1a69abd968007f
serjoradpytnio <serjoradpytnio@mail.ru>
2022-12-17 19:26:14 +0300

    Add test_circle.py

--- main.py
+++
@@ -1,3 +0,0 @@
-print("Hello World!")
-x = 5
-print(x)
---
+++ test_circle.py
@@ -0,0 +1,10 @@
+import unittest
+from circle import circle_area
+from cmath import pi
+
+class TestCircleArea(unittest.TestCase):
+    def test_area(self):
+        self.assertEqual(circle_area(3),pi*3**2)
+        self.assertEqual(circle_area(1),pi)
+        self.assertEqual(circle_area(0),0)
+        self.assertEqual(circle_area(2,5),pi*2,5**2)
